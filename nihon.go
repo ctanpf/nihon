dde4876e51aa67e8843e548213e3e8b7d83543d9
cliff <clifftanpeifeng@gmail.com>
2018-12-26 21:58:15 -0600

    Added katakana and merge func

--- nihon.go
+++ nihon.go
@@ -138,3 +138,150 @@ var RomajiToJapaneseMap = map[string]string{
 	"りゅ": "ryu",
 	"りょ": "ryo",
 }
+
+var KatakanaToJapaneseMap = map[string]string{
+	"ア": "a",
+	"イ": "i",
+	"ウ": "u",
+	"エ": "e",
+	"オ": "o",
+
+	"カ": "ka",
+	"キ": "ki",
+	"ク": "ku",
+	"ケ": "ke",
+	"コ": "ko",
+
+	"サ": "sa",
+	"シ": "shi",
+	"ス": "su",
+	"セ": "se",
+	"ソ": "so",
+
+	"タ": "ta",
+	"チ": "chi",
+	"ツ": "tsu",
+	"テ": "te",
+	"ト": "to",
+
+	"ナ": "na",
+	"ニ": "ni",
+	"ヌ": "nu",
+	"ネ": "ne",
+	"ノ": "no",
+
+	"ハ": "ha",
+	"ヒ": "hi",
+	"フ": "fu",
+	"へ": "he",
+	"ホ": "ho",
+
+	"マ": "ma",
+	"ミ": "mi",
+	"ム": "mu",
+	"メ": "me",
+	"モ": "mo",
+
+	"ラ": "ra",
+	"リ": "ri",
+	"ル": "ru",
+	"レ": "re",
+	"ロ": "ro",
+
+	"ガ": "ga",
+	"ギ": "gi",
+	"グ": "gu",
+	"ゲ": "ge",
+	"ゴ": "go",
+
+	"ザ": "za",
+	"ジ": "ji",
+	"ズ": "zu",
+	"ゼ": "ze",
+	"ゾ": "zo",
+
+	"ダ": "da",
+	"ヂ": "ji",
+	"ヅ": "zu",
+	"デ": "de",
+	"ド": "do",
+
+	"バ": "ba",
+	"ビ": "bi",
+	"ブ": "bu",
+	"ベ": "be",
+	"ボ": "bo",
+
+	"パ": "pa",
+	"ピ": "pi",
+	"プ": "pu",
+	"ペ": "pe",
+	"ポ": "po",
+
+	"ヤ": "ya",
+	"ユ": "yu",
+	"ヨ": "yo",
+
+	"ワ": "wa",
+	"ヲ": "wo",
+	"ン": "n",
+
+	"キャ": "kya",
+	"キュ": "kyu",
+	"キョ": "kyo",
+
+	"ギャ": "gya",
+	"ギュ": "gyu",
+	"ギョ": "gyo",
+
+	"シャ": "sha",
+	"シュ": "shu",
+	"ショ": "sho",
+
+	"ジャ": "ja",
+	"ジュ": "ju",
+	"ジョ": "jo",
+
+	"チャ": "cha",
+	"チュ": "chu",
+	"チェ": "cho",
+
+	"ニャ": "nya",
+	"ニュ": "nyu",
+	"ニョ": "nyo",
+
+	"ヒャ": "hya",
+	"ヒュ": "hyu",
+	"ヒョ": "hyo",
+
+	"ビャ": "bya",
+	"ビュ": "byu",
+	"ビョ": "byo",
+
+	"ピャ": "pya",
+	"ピュ": "pyu",
+	"ピョ": "pyo",
+
+	"ミャ": "mya",
+	"ミュ": "myu",
+	"ミョ": "myo",
+
+	"リャ": "rya",
+	"リュ": "ryu",
+	"リョ": "ryo",
+}
+
+func AllHiragana() map[string]string {
+	return merge(RomajiToJapaneseMap, KatakanaToJapaneseMap)
+}
+
+func merge(a map[string]string, b map[string]string) map[string]string {
+	res := make(map[string]string)
+	for k, v := range a {
+		res[k] = v
+	}
+	for k, v := range b {
+		res[k] = v
+	}
+	return res
+}
